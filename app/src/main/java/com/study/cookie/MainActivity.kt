d55f5e1caba6618783df46f2fe1f1b64c5318624
someh2705 <someh2705@gmail.com>
2022-01-01 00:13:17 +0900

    init: Lifecycle 과 Viewmodel, Hilt 라이브러리 추가

--- app/src/main/java/com/study/cookie/MainActivity.kt
+++ app/src/main/java/com/study/cookie/MainActivity.kt
@@ -2,12 +2,81 @@ package com.study.cookie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.lifecycle.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.yield
+import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycleScope.launchWhenCreated {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.count.collect {
+                    Log.d("MainActivity", "$it")
+                }
+            }
+        }
+    }
+}
+
+
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    testRepository: TestRepository
+): ViewModel() {
+    private val _count = MutableStateFlow<Int>(0)
+    val count = _count.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            launch {
+                val number = testRepository.fetchCount()
+                _count.value = number
+            }
+            launch {
+                while (true) {
+                    delay(500L)
+                    _count.value++
+                }
+            }
+        }
+    }
+}
+
+class TestRepository {
+    suspend fun fetchCount(): Int {
+        delay(2000L)
+        return 1000
+    }
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+class TestModule {
+
+    @Singleton
+    @Provides
+    fun provideTestRepository(): TestRepository {
+        return TestRepository()
     }
 }
