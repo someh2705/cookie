aa26e0a5b8eb1f4e4d0257afea52f3cad475402d
someh2705 <someh2705@gmail.com>
2022-01-02 03:00:15 +0900

    init: Retrofit2 와 Moshi 라이브러리 추가

--- app/src/main/java/com/study/cookie/MainActivity.kt
+++ app/src/main/java/com/study/cookie/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.lifecycle.*
+import com.squareup.moshi.Json
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,8 +17,12 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.yield
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 import javax.inject.Inject
+import javax.inject.Named
 import javax.inject.Singleton
 
 @AndroidEntryPoint
@@ -30,43 +35,55 @@ class MainActivity : AppCompatActivity() {
 
         lifecycleScope.launchWhenCreated {
             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.count.collect {
-                    Log.d("MainActivity", "$it")
+                viewModel.cookieList.collect {
+                    it.cookieList.forEach {
+                        Log.d("MainActivity", "$it")
+                    }
                 }
             }
         }
     }
 }
-
 
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     testRepository: TestRepository
-): ViewModel() {
-    private val _count = MutableStateFlow<Int>(0)
-    val count = _count.asStateFlow()
+) : ViewModel() {
+    private val _cookieList = MutableStateFlow<CookieInfoList>(
+        CookieInfoList(
+            cookieList = emptyList(),
+            last = 0,
+            next = true
+        )
+    )
+    val cookieList = _cookieList.asStateFlow()
 
     init {
         viewModelScope.launch {
             launch {
-                val number = testRepository.fetchCount()
-                _count.value = number
-            }
-            launch {
                 while (true) {
-                    delay(500L)
-                    _count.value++
+                    delay(1000L)
+                    cookieList.value.last?.let {
+                        val fetchCookieList = testRepository.fetchCookieList(
+                            it,
+                            length = 4
+                        )
+
+                        _cookieList.value = fetchCookieList
+                    }
                 }
             }
         }
     }
 }
 
-class TestRepository {
-    suspend fun fetchCount(): Int {
-        delay(2000L)
-        return 1000
+@Singleton
+class TestRepository @Inject constructor(
+    private val cookieService: CookieService
+) {
+    suspend fun fetchCookieList(start: Int, length: Int): CookieInfoList {
+        return cookieService.getCookieInfoList(start, length)
     }
 }
 
@@ -76,7 +93,64 @@ class TestModule {
 
     @Singleton
     @Provides
-    fun provideTestRepository(): TestRepository {
-        return TestRepository()
+    fun provideTestRepository(cookieService: CookieService): TestRepository {
+        return TestRepository(cookieService)
     }
+
+    @Singleton
+    @Provides
+    @Named("baseUrl")
+    fun provideBaseUrl(): String {
+        return "http://172.27.176.1:8000"
+    }
+
+    @Singleton
+    @Provides
+    fun provideMoshiConverterFactory(): MoshiConverterFactory {
+        return MoshiConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        moshiConverterFactory: MoshiConverterFactory,
+        @Named("baseUrl") baseUrl: String
+    ): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(moshiConverterFactory)
+            .baseUrl(baseUrl)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideCookieService(retrofit: Retrofit): CookieService {
+        return retrofit.create(CookieService::class.java)
+    }
+}
+
+data class CookieInfoList(
+    @field:Json(name = "list")
+    val cookieList: List<CookieInfo>,
+    @field:Json(name = "last")
+    val last: Int?,
+    @field:Json(name = "next")
+    val next: Boolean
+)
+
+data class CookieInfo(
+    @field:Json(name = "cookie_id")
+    val cookieId: Int,
+    @field:Json(name = "cookie_name")
+    val cookieName: String,
+    @field:Json(name = "cookie_image")
+    val cookieImage: String
+)
+
+interface CookieService {
+    @GET("/cookie/v1/cookie_id/list")
+    suspend fun getCookieInfoList(
+        @Query("start") start: Int,
+        @Query("length") length: Int
+    ): CookieInfoList
 }
